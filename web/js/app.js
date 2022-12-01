1331835fc2693d364724c8efa138c96612531c69
D Marques <diogomarquesbhdm19@gmail.com>
2022-12-01 12:23:05 -0300

    ultimas correcoes

--- web/js/app.js
+++ web/js/app.js
@@ -49,51 +49,6 @@ const getDados = () => {
                 newScreen.innerHTML = texto;
             }
         })
-
-    // for (let i = 0; i < dados.articles.length; i++) {
-    //     let noticia = dados.articles[i];
-
-    //     let data = new Date(noticia.publishedAt);
-
-    //     texto += `
-    //     <article class="boxNoticias">
-    //     <img src="${noticia.urlToImage}" alt="Imagem da notícia">
-    //     <span class="titulo">${noticia.title}</span><br>
-    //     <span class="creditos">${data.toLocaleDateString()} - ${noticia.source.name} - ${noticia.author}</span><br>
-    //     <span class="text">${noticia.content}
-    //     <a href="${noticia.url}" target="_blank">Leia mais ...</a>
-    //     </span>
-    //     <p>===============================</p>
-    //     </article>
-    //     `;
-    // };
-    // Preencher a div com o texto HTML
-    // let loading = document.querySelector('.loader')
-    // loading.style.display = 'none'
 }
 
 getDados();
-
-// function pesquisar(evento) {
-//     evento.preventDefault();
-//     let query = document.getElementById('campoDePesquisa').value;
-
-//     let newTela = document.getElementById('tela');
-
-//     if (!query) {
-//         newTela.innerHTML = '<h3>Por favor, insira alguma categoria de notícia</h3>';
-//         return;
-//     }
-
-//     newTela.innerHTML = '';
-
-//     let loading = document.querySelector('.loader');
-//     loading.style.display = 'flex';
-
-//     let xhr = new XMLHttpRequest();
-//     xhr.onload = exibirNoticias;
-//     xhr.open('GET', `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`);
-//     xhr.send();
-// }
-
-// document.getElementById('btn').addEventListener('click', pesquisar);
